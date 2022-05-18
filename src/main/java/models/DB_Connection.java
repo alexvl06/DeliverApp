939951d22b9877a0075904ab9ef068238f196508
Alexis Avila <alexis.avila@inter-telco.com>
2022-05-18 16:59:26 -0500

    Database connection was implemented and CRUD methods were developed excluding update

---
+++ src/main/java/models/DB_Connection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author inter-telco
+ */
+public class DB_Connection {
+
+    public Connection get_connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/deliver_db", "root", "Exito2018");
+
+        } catch (SQLException e) {
+
+            System.out.println(e);
+        }
+        return connection;
+    }
+
+}
--- src/main/java/models/LegalModel.java
+++ src/main/java/models/LegalModel.java
@@ -5,6 +5,11 @@
 package models;
 
 import classes.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -18,8 +23,8 @@ public class LegalModel {
     public static ArrayList<Legal> getLegalList() {
         return legalList;
     }
-    
-    public static void defaultLegalList(){
+
+    public static void defaultLegalList() {
         legalList.add(new Legal("Inter-Telco S.A.S.", "900.382.516-1", "Edificio Prisma, Cra 35A N° 15B-35, Oficina 212, Medellín, Antioquia", "soporte@inter-telco.com", "1", 8500.0, "5200500"));
         legalList.add(new Legal("SUMMA-sci S.A.S.", "901.033.369-2", "CALLE 51 SUR 48 57 MAYORCA ETAPA 3 TORRE NORTE PISO 12, MEDELLIN, ANTIOQUIA", "shappy@summa-sci.com", "2", 12500.0, "6046398"));
     }
@@ -27,38 +32,105 @@ public class LegalModel {
     private LegalModel() {
     }
 
-
     //CRUD
     public static Legal getOneClient(String id) {
-        try {
-            for (int i = 0; i < LegalModel.legalList.size(); i++) {
-                if (LegalModel.legalList.get(i).getId().equals(id)) {
 
-                    return LegalModel.legalList.get(i);
-                }
+        DB_Connection db_connect = new DB_Connection();
+        try ( Connection connection = db_connect.get_connection()) {
+            PreparedStatement ps;
+            ResultSet rsLegal;
+            ResultSet rsClient;
+            try {
+                String query = "select * from Legals where id = ?";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, id);
+                rsLegal = ps.executeQuery();
+                ps.close();
+                query = "select * from Clients where id = ?";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, rsLegal.getString("idClient"));
+                rsClient = ps.executeQuery();
+                Legal legal = new Legal(rsLegal.getString("business name"), rsLegal.getString("NIT"), rsClient.getString("address")
+                , rsClient.getString("email"), Integer.toString(rsClient.getInt("idClient"))
+                , rsClient.getDouble("money"), rsClient.getString("phoneNumber")
+                );
+
+                ps.close();
+                
+                return legal;
+            } catch (SQLException e) {
+                System.out.println("Legal client was not recovered due to a fatal error has occurred");
+                System.out.println(e);
+                return null;
             }
-        } catch (Exception e) {
-            System.out.println("Client not found: " + e);
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
         }
-        return null;
+
+
 
     }
 
     public static boolean deleteLegal(String id) {
-        for (int i = 0; i < LegalModel.legalList.size(); i++) {
-            if (LegalModel.legalList.get(i).getId().equals(id)) {
-
-                LegalModel.legalList.remove(i);
-                for (int j = i; j < LegalModel.legalList.size(); j++) {
-                    LegalModel.legalList.get(j).setId(Integer.toString(Integer.parseInt(LegalModel.legalList.get(j).getId()) - 1));
-                }
+        DB_Connection db_connect = new DB_Connection();
+        try ( Connection conexion = db_connect.get_connection()) {
+            PreparedStatement ps;
+            try {
+                String query = "delete from Legals where message_id = ?";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, id);
+                ps.executeUpdate();
+                
+                ps.close();
+                System.out.println("The client was deleted successfully");
                 return true;
+            } catch (SQLException e) {
+                System.out.println("Client was not deleted due to a fatal error has occurred");
+                System.out.println(e);
             }
+        } catch (SQLException e) {
+            System.out.println(e);
         }
+        
         return false;
     }
 
     public static void createLegal(Legal legal) {
+        DB_Connection db_connect = new DB_Connection();
+        try ( Connection conexion = db_connect.get_connection()) {
+            PreparedStatement ps;
+            try {
+                String query = "insert into Clients (address, phoneNumber, email, money) values (?, ?, ?, ?)";
+                ps = conexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, legal.getAddress());
+                ps.setString(2, legal.getPhoneNumber());
+                ps.setString(3, legal.getEmail());
+                ps.setDouble(4, legal.getMoney());
+                int rowsInserted = ps.executeUpdate();
+                if(rowsInserted>0){
+                    ResultSet generatedKeys = ps.getGeneratedKeys();
+                    if(generatedKeys.next()){
+                        int idClient = generatedKeys.getInt(1);
+                        query = "insert into Legals (NIT, business name, idClient) values (?, ?, ?)";
+                        ps = conexion.prepareStatement(query);
+                        ps.setString(1, legal.getNIT());
+                        ps.setString(2, legal.getBusinessName());
+                        ps.setInt(3, idClient);
+                        ps.executeUpdate();
+                        ps.close();
+                    
+                    }
+                }
+                System.out.println("¡New client was created successfully!");
+                ps.close();
+            } catch (SQLException e) {
+                System.out.println("The client was not created due to a fatal error has occurred");
+                System.out.println(e);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
         LegalModel.legalList.add(legal);
     }
 
